266de7b78dcf9455b5d148058c89e98957a3af05
Anushka Singh <anushkasingh117733@gmail.com>
2022-03-22 22:54:19 +0530

    Update circularllinsert.c

--- circularllinsert.c
+++ circularllinsert.c
@@ -26,3 +26,21 @@ struct Node *addToEmpty(struct Node *last, int data)
 2. Make T -> next = last -> next. 
 3. last -> next = T. */
 
+struct Node *addBegin(struct Node *last, int data)
+{
+if (last == NULL)
+	return addToEmpty(last, data);
+
+// Creating a node dynamically.
+struct Node *temp
+		= (struct Node *)malloc(sizeof(struct Node));
+
+// Assigning the data.
+temp -> data = data;
+
+// Adjusting the links.
+temp -> next = last -> next;
+last -> next = temp;
+
+return last;
+}
