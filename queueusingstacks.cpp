0506302adcef0c606a6f34e031f28f223d2b25f9
Anushka Singh <anushkasingh117733@gmail.com>
2022-04-01 22:33:22 +0530

    Update queueusingstacks.cpp

--- queueusingstacks.cpp
+++ queueusingstacks.cpp
@@ -54,3 +54,23 @@ int main()
 
 	return 0;
 }
+
+
+
+
+
+
+Output: 
+
+1
+2
+3
+  Complexity Analysis: 
+
+Time Complexity: 
+Push operation: O(N). 
+In the worst case we have empty whole of stack 1 into stack 2.
+Pop operation: O(1). 
+Same as pop operation in stack.
+Auxiliary Space: O(N). 
+Use of stack for storing values.
