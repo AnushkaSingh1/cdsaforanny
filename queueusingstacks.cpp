8a93d2cf3b77bed598e67d7668154d1e779df345
Anushka Singh <anushkasingh117733@gmail.com>
2022-04-01 22:35:16 +0530

    Update queueusingstacks.cpp

--- queueusingstacks.cpp
+++ queueusingstacks.cpp
@@ -71,6 +71,6 @@ int main()
 //Push operation: O(N). 
 //In the worst case we have empty whole of stack 1 into stack 2.
 //Pop operation: O(1). 
-Same as pop operation in stack.
-Auxiliary Space: O(N). 
-Use of stack for storing values.
+//Same as pop operation in stack.
+//Auxiliary Space: O(N). 
+//Use of stack for storing values.
